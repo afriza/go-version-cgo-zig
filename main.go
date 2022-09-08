fa2094f56a152609abae181caaa9ac833e8df169
Afriza N. Arief <afriza.na@gmail.com>
2022-09-08 16:55:15 +0700

    remove irrelevant `params`

--- main.go
+++ main.go
@@ -10,14 +10,7 @@ import (
 )
 
 func main() {
-	params := "?_pragma=foreign_keys(1)" +
-		"&_pragma=journal_mode(WAL)" +
-		"&_pragma=synchronous(NORMAL)" +
-		"&_pragma=busy_timeout(5000)" +
-		"&_pragma=journal_size_limit(200000000)" +
-		"&_txlock=immediate" +
-		""
-	db, err := sql.Open("sqlite3", "file::memory:"+params)
+	db, err := sql.Open("sqlite3", "file::memory:")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -36,5 +29,4 @@ func main() {
 			fmt.Println(s.Key+":", s.Value)
 		}
 	}
-
 }
